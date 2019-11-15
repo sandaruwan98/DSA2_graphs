580b53fa0779aea4e8001b010b9bdfaca85a7365
sandaruwan98 <52200670+sandaruwan98@users.noreply.github.com>
2019-11-15 19:12:45 +0530

    Depth frist search Implemented

--- graphs/listimp.cpp
+++ graphs/listimp.cpp
@@ -1,54 +1,51 @@
 
 #include <iostream>
-#include <list> 
+#include <list>
 using namespace std;
-// struct node
-// {
-//     list<int> vertex;
-//     int val;
-// };
 
-
-
-class Graph{
+class Graph
+{
     list<int> *gph;
     int v;
-    
-    public:
+
+public:
     Graph(int v);
-    void AddNode(int v,int w);
+    void AddNode(int v, int w);
     void BFS(int s);
+    void DFS();
+    void Visit(int v, char *visited);
 };
 
-Graph::Graph(int v){
+Graph::Graph(int v)
+{
     this->v = v;
     gph = new list<int>[v];
 }
 
-void Graph::AddNode(int v,int w){
+void Graph::AddNode(int v, int w)
+{
     gph[v].push_back(w);
 }
 
-void Graph::BFS(int s){
+void Graph::BFS(int s)
+{
     int *visited = new int[v];
     int i;
-    for ( i = 0; i < v; i++)
+    for (i = 0; i < v; i++)
     {
-        visited[i]=0;
+        visited[i] = 0;
     }
-    
+
     list<int> queue;
     queue.push_back(s);
-    
-    
 
     std::list<int>::iterator j;
     while (!queue.empty())
     {
         s = queue.front();
-        //print 
+        //print
         cout << s << endl;
-        for ( j = gph[s].begin(); j != gph[s].end(); ++j)
+        for (j = gph[s].begin(); j != gph[s].end(); ++j)
         {
             if (visited[*j] == 0)
             {
@@ -58,21 +55,52 @@ void Graph::BFS(int s){
         }
 
         queue.pop_front();
-        
     }
-    
+}
 
+void Graph::DFS()
+{
+    char *visited = new char[v];
+    int i;
+    for (i = 0; i < v; i++)
+        visited[i] = 'W';
 
+    for (i = 0; i < v; i++)
+        if (visited[i] == 'W')
+            Visit(i, visited);
+}
+
+void Graph::Visit(int v, char *visited)
+{
+    visited[v] = 'G';
+
+    list<int>::iterator i;
+    for (i = gph[v].begin(); i != gph[v].end(); ++i)
+    {
+        if (visited[*i] == 'W')
+        {
+            Visit(*i, visited);
+        }
+    }
+
+    cout << v << endl;
 }
 
 int main()
 {
     Graph g(4);
-    g.AddNode(0,1);
-    g.AddNode(1,3);
-    g.AddNode(2,1);
-    g.AddNode(3,0);
+    g.AddNode(0, 1);
+    g.AddNode(1, 3);
+    g.AddNode(2, 1);
+    g.AddNode(3, 0);
+    cout << endl << "BFS" << endl;
     g.BFS(2);
+    cout << endl << "DFS" << endl;
+    g.DFS();
     return 0;
 }
 
+// 0   1
+// 1   3
+// 2   1
+// 3   0
--- graphs/listimpnn.cpp
+++ graphs/listimpnn.cpp
@@ -1,75 +1,83 @@
 
 #include <iostream>
-#include <list> 
+#include <list>
 using namespace std;
 class node
-{   
-    public:
-    node(int v){
-        val=v;
+{
+public:
+    node(int v)
+    {
+        val = v;
         // vertex.push_back(w);
     }
     list<int> vertex;
     int val;
 };
 
-
-
-class Graph{
+class Graph
+{
     list<node> gph;
     int v;
-    
-    public:
+
+public:
     Graph(int v);
     void AddNode(int v);
-    void AddEdge(int source,int dest);
+    void AddEdge(int source, int dest);
     void BFS(int s);
+    void DFS(int s);
 };
 
-Graph::Graph(int v){
+Graph::Graph(int v)
+{
     this->v = v;
-   
 }
 
-void Graph::AddNode(int val){
-    node *tmp = new node(val); 
+void Graph::AddNode(int val)
+{
+    node *tmp = new node(val);
     // gph[v].push_back(w);
     gph.push_back(*tmp);
-//    cout << gph.back().val << " ";
+    //    cout << gph.back().val << " ";
 }
 
-void  Graph::AddEdge(int source,int dest){
-    
-    list<node>::iterator i=gph.begin();
-    while (source != (i->val)){++i;}
-    
+void Graph::AddEdge(int source, int dest)
+{
+
+    list<node>::iterator i = gph.begin();
+    while (source != (i->val))
+    {
+        ++i;
+    }
+
     i->vertex.push_back(dest);
     cout << i->val << " - " << i->vertex.back() << endl;
 }
 
-void Graph::BFS(int s){
+void Graph::BFS(int s)
+{
     int *visited = new int[v];
     int j;
-    for ( j = 0; j < v; j++)
+    for (j = 0; j < v; j++)
     {
-        visited[j]=0;
+        visited[j] = 0;
     }
-    
+
     list<int> queue;
     queue.push_back(s);
-    
-    
 
     std::list<int>::iterator i;
     while (!queue.empty())
     {
         s = queue.front();
         visited[s] = 1;
-        list<node>::iterator k=gph.begin();
-         while (s != (k->val)){++k;}
-        //print 
+        list<node>::iterator k = gph.begin();
+        while (s != (k->val))
+        {
+            ++k;
+        }
+        //print
         cout << s << endl;
-        for ( i = k->vertex.begin(); i != k->vertex.end(); ++i)
+        for (i = k->vertex.begin(); i != k->vertex.end(); ++i)
         {
             if (visited[*i] == 0)
             {
@@ -79,16 +87,17 @@ void Graph::BFS(int s){
         }
 
         queue.pop_front();
-        
     }
-      cout << endl;   
-    for ( j = 0; j < v; j++)
+    cout << endl;
+    for (j = 0; j < v; j++)
     {
-       
+
         cout << visited[j] << " ";
     }
-    
+}
 
+void Graph::DFS(int s)
+{
 }
 
 int main()
@@ -97,14 +106,15 @@ int main()
     g.AddNode(0);
     g.AddNode(1);
     g.AddNode(2);
+
     g.AddNode(3);
- 
-    g.AddEdge(0,1);
-    g.AddEdge(1,3);
-    g.AddEdge(2,1);
-    g.AddEdge(3,0);
-    cout << endl << endl;
+
+    g.AddEdge(0, 1);
+    g.AddEdge(1, 3);
+    g.AddEdge(2, 1);
+    g.AddEdge(3, 0);
+    cout << endl
+         << endl;
     g.BFS(2);
     return 0;
 }
-
